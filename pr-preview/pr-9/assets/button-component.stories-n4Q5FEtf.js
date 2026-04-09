56f36605f5c5e429d5ffe5f22aeed5ea2e149669
matthewhuntsberry <matthewhuntsberry@users.noreply.github.com>
2026-04-09 22:28:16 +0000

    Deploy preview for PR 9 🛫

---
+++ pr-preview/pr-9/assets/button-component.stories-n4Q5FEtf.js
@@ -0,0 +1,10 @@
+import{x as e}from"./iframe-BXJKph0-.js";import"./preload-helper-CGRQIgFS.js";const r=(o={})=>{const{label:n="ButtonComponent"}=o;return e`
+    <div class="button-component">
+      <span class="button-component__label">${n}</span>
+    </div>
+  `},p={title:"Prototyping/matthewhuntsberry/ButtonComponent",tags:["autodocs"],argTypes:{label:{control:"text"}}},t={args:{label:"ButtonComponent"},render:o=>r(o)};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "ButtonComponent"
+  },
+  render: args => ButtonComponent(args)
+}`,...t.parameters?.docs?.source}}};const l=["Default"];export{t as Default,l as __namedExportsOrder,p as default};
---
+++ pr-preview/pr-9/assets/formatter-EIJCOSYU-C471smVz.js
@@ -0,0 +1,1 @@
+import{d as m,a as e}from"./blocks-DG9rwOon.js";import{b as o}from"./iframe-BXJKph0-.js";import"./preload-helper-CGRQIgFS.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
