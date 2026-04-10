e043ee9c9b0c00044e08db6f56ee37bd3c2c5a45
matthewhuntsberry <matthewhuntsberry@users.noreply.github.com>
2026-04-10 16:14:09 +0000

    Deploy preview for PR 11 🛫

---
+++ pr-preview/pr-11/assets/test-proto-1.stories-D8p8ygJD.js
@@ -0,0 +1,10 @@
+import{x as o}from"./iframe-Drkr0HCq.js";import"./preload-helper-Cjy7b9qv.js";const s=(r={})=>{const{label:e="TestProto1"}=r;return o`
+    <div class="test-proto-1">
+      <span class="test-proto-1__label">${e}</span>
+    </div>
+  `},l={title:"Prototyping/matthewhuntsberry/TestProto1",tags:["autodocs"],argTypes:{label:{control:"text"}}},t={args:{label:"TestProto1"},render:r=>s(r)};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "TestProto1"
+  },
+  render: args => TestProto1(args)
+}`,...t.parameters?.docs?.source}}};const c=["Default"];export{t as Default,c as __namedExportsOrder,l as default};
