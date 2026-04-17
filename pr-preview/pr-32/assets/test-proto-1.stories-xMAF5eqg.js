4eb9fd4885dc740aadae9765bb90fa2cc30645ab
matthewhuntsberry <matthewhuntsberry@users.noreply.github.com>
2026-04-17 20:48:24 +0000

    Remove preview for PR 32 🛬

--- pr-preview/pr-32/assets/test-proto-1.stories-xMAF5eqg.js
+++
@@ -1,6 +0,0 @@
-import{x as e}from"./iframe-Dmq61beB.js";import"./preload-helper-BbL0o52_.js";function o({label:r="TestProto1"}={}){return e`<div class="test-proto-1">${r}</div>`}const n={title:"Prototyping/matthewhuntsberry/TestProto1",tags:["autodocs"],argTypes:{label:{control:"text"}}},t={args:{label:"TestProto1"},render:r=>o(r)};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: "TestProto1"
-  },
-  render: args => TestProto1(args)
-}`,...t.parameters?.docs?.source}}};const c=["Default"];export{t as Default,c as __namedExportsOrder,n as default};
