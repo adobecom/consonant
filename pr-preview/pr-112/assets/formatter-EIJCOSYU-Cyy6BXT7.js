8325c3ba6958269d649e961830dc702e435583c8
matthewhuntsberry <matthewhuntsberry@users.noreply.github.com>
2026-08-14 21:22:12 +0000

    Remove preview for PR 112 🛬

--- pr-preview/pr-112/assets/formatter-EIJCOSYU-Cyy6BXT7.js
+++
@@ -1,1 +0,0 @@
-import{d as m,b as e}from"./DocsRenderer-LL677BLK-DHjoUC2c.js";import{O as o}from"./iframe-BG7cM0ts.js";import"./preload-helper-BUOwyZq8.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
--- pr-preview/pr-112/assets/section-header-DbngvqLG.js
+++
@@ -1,8 +0,0 @@
-import{x as s}from"./iframe-BG7cM0ts.js";import{R as d}from"./rich-content-CWGzAKr4.js";const m=({eyebrow:e="",showEyebrow:o=!0,title:i="",body:n="",theme:t="on-light",showActions:r=!1,actions:a}={})=>s`
-  <div
-    class="c-section-header"
-    data-theme=${t==="on-dark"?"on-dark":"on-light"}
-  >
-    ${d({theme:t,density:"tight",justifyContent:"center",measure:"wide",eyebrow:e,showEyebrow:o,title:i,body:n,showActions:r,actions:a})}
-  </div>
-`;export{m as S};
