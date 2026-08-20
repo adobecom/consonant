581bdcb01f67d38d3c80fa07991170de142bdc66
matthewhuntsberry <matthewhuntsberry@users.noreply.github.com>
2026-08-20 14:05:38 +0000

    Remove preview for PR 131 🛬

--- pr-preview/pr-131/assets/section-header-DakoViCd.js
+++
@@ -1,8 +0,0 @@
-import{x as s}from"./iframe-B2X2aCfh.js";import{R as d}from"./rich-content-AcXz4BTV.js";const m=({eyebrow:e="",showEyebrow:o=!0,title:i="",body:n="",theme:t="on-light",showActions:r=!1,actions:a}={})=>s`
-  <div
-    class="c-section-header"
-    data-theme=${t==="on-dark"?"on-dark":"on-light"}
-  >
-    ${d({theme:t,density:"tight",justifyContent:"center",measure:"wide",eyebrow:e,showEyebrow:o,title:i,body:n,showActions:r,actions:a})}
-  </div>
-`;export{m as S};
