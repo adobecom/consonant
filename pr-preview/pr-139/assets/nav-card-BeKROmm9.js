d70c30ce700658131070aa30b801f5629907d265
matthewhuntsberry <matthewhuntsberry@users.noreply.github.com>
2026-08-20 20:30:09 +0000

    Deploy preview for PR 139 🛫

---
+++ pr-preview/pr-139/assets/nav-card-BeKROmm9.js
@@ -0,0 +1,16 @@
+import{E as c,x as a}from"./iframe-B2hp8VXM.js";import{o as p}from"./unsafe-html-CAZmCvIp.js";import{N as i}from"./nav-card-shell-akfs4O2-.js";import{c as e}from"./chevron-right-CnbUzkxe.js";const $=p(e),x=({eyebrow:s="",title:n="",body:r="",ctaLinkLabel:o="",ctaLinkHref:_="",ctaButtonLabel:v="Explore",ctaButtonHref:t="",interactive:d=!0}={})=>{const l=a`
+    <div class="c-nav-card__top">
+      ${s?a`<p class="c-nav-card__eyebrow">${s}</p>`:c}
+      <div class="c-nav-card__content">
+        ${n?a`<h3 class="c-nav-card__title">${n}</h3>`:c}
+        <div class="c-nav-card__body-area">
+          ${r?a`<p class="c-nav-card__body">${r}</p>`:c}
+          ${o&&_?a`
+                <a class="c-nav-card__cta-link" href=${_}>
+                  <span class="c-nav-card__cta-link__label">${o}</span>
+                  <span class="c-nav-card__cta-link__icon">${$}</span>
+                </a>`:c}
+        </div>
+      </div>
+    </div>
+  `;return i({className:"c-nav-card",content:l,ctaButtonLabel:v,ctaButtonHref:t,interactive:d})};export{x as N};
